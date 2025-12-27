d531a95e074dc7f5773c6528abd9103e5771ba1d
Umer Safeer Lodhi <umersafeerlodhi2@gmail.com>
2025-12-27 17:46:59 +0500

    save delete changes

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,6 @@ import userRoutes from './routes/user.routes.js'
 import categoryRoutes from './routes/category.routes.js'
 import { errorHandler } from './middleware/errorHandler.js'
 import cors from 'cors'
-import path from 'path';
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 import { v2 as cloudinary } from 'cloudinary'
 import { createAdmin } from './createAdmin.js'
 dotenv.config();
@@ -34,8 +30,6 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_SECRET_KEY
 })
 
-
-
 databaseConnection();
 createAdmin();
 app.use(cookieParser());
@@ -44,17 +38,6 @@ app.use(express.urlencoded({limit: '50mb', extended: true}))
 app.use(productRoutes);
 app.use(userRoutes);
 app.use(categoryRoutes);
-
-app.use(express.static(path.join(__dirname, 'eshopper-frontend/build')));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'eshopper-frontend/build', 'index.html'));
-});
-
-// Serve admin panel
-app.use('/admin', express.static(path.join(__dirname, 'eshopper-admin-panel/build')));
-app.get('/admin/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'eshopper-admin-panel/build', 'index.html'));
-});
 
 app.use(errorHandler);
 
