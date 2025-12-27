80b88b7764ce965e32c21f1c8698e5b1a3483985
Umer Safeer Lodhi <umersafeerlodhi2@gmail.com>
2025-12-27 08:58:45 +0500

    second commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DATABASE_URL);
-        console.log("Database is connected")
-    } catch (error) {
-        console.log(error || "something went wrong")
-    }
-}
+  try {
+    await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database is connected");
+  } catch (error) {
+    console.log(error || "something went wrong");
+  }
+};
 
 export default connectDB;
