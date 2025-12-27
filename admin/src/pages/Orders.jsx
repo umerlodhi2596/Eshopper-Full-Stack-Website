f799c8b365534f6003e4cfa675dd30a55e0047a8
Umer Safeer Lodhi <umersafeerlodhi2@gmail.com>
2025-12-27 09:24:56 +0500

    commit

---
+++ admin/src/pages/Orders.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Orders() {
+  return (
+    <>
+      <h3>Orders</h3>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi fuga similique dolorem, perferendis repellat recusandae architecto nemo quasi, dolorum eligendi magni consequatur voluptatum illo nisi excepturi rem mollitia asperiores quas deleniti reiciendis dolore? Quia, tenetur debitis ipsam eligendi necessitatibus earum nihil unde eum perferendis assumenda, non iste porro dolore at, excepturi facere architecto placeat! Voluptas expedita, nihil incidunt molestiae recusandae ullam quidem quia id quae quasi repellendus? Necessitatibus accusantium consequatur ipsa vitae quas assumenda sequi fuga a molestiae cupiditate sed modi rerum esse deserunt temporibus iusto error, dicta facilis tempora aperiam dignissimos blanditiis. Ipsum tempora similique eius architecto quo, aliquid labore itaque sunt nulla in facilis soluta quos blanditiis quis doloribus quia voluptas cumque at ducimus consectetur expedita quam eligendi, eaque suscipit. Autem, vitae officiis. Magni dolore porro expedita aperiam, quas voluptates veritatis provident corrupti optio numquam! Nam eum autem voluptate quia provident quas debitis corrupti ducimus, ut non impedit aliquam maxime quibusdam esse ab fugit maiores totam sit sapiente. Aspernatur recusandae alias commodi voluptatem quas architecto ab, quae ducimus deserunt corporis neque, pariatur placeat fugit a excepturi harum. Voluptatem magnam, doloremque, expedita excepturi quae, molestiae ad alias esse ipsa blanditiis beatae. Debitis corporis adipisci animi beatae doloremque velit molestiae!</p>
+    </>
+  )
+}
+
+export default Orders
--- backend/config/db.js
+++ backend/config/db.js
@@ -1,25 +1,12 @@
 import mongoose from "mongoose";
 
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function connectDB() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.DATABASE_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }).then((mongoose) => mongoose);
-  }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URL);
+        console.log("Database is connected")
+    } catch (error) {
+        console.log(error || "something went wrong")
+    }
 }
 
 export default connectDB;
