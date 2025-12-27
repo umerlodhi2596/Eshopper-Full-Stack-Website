6ffa155e7439ae9ea680ebab442959d67a7dbaaa
Umer Safeer Lodhi <umersafeerlodhi2@gmail.com>
2025-12-27 15:38:05 +0500

    change url

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174' ],
+  origin: ['https://eshopper-frontend.onrender.com/', 'http://localhost:5174' ],
   credentials: true,
 }
 
