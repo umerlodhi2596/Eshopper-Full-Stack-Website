669ba9fc1595aa73e4e812f9fbab4cf07b00a956
Umer Safeer Lodhi <umersafeerlodhi2@gmail.com>
2025-12-27 17:58:54 +0500

    admin change route

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           }}
         />
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
           <Route
             path="/"
             element={
